ab4d8acb65f950c53edb804c60ff34504154d60b
Nicolò Andreola <nicolo.andreola@studenti.unicam.it>
2026-01-07 13:13:23 +0100

    Consegna Esercitazione 11 (MatrixMultiplication)

--- src/main/java/it/unicam/cs/asdl2526/es11/MatrixMultiplicationSolver.java
+++ src/main/java/it/unicam/cs/asdl2526/es11/MatrixMultiplicationSolver.java
@@ -3,7 +3,7 @@ package it.unicam.cs.asdl2526.es11;
 import java.util.List;
 
 /**
- * Un sover prende una certa sequenza di matrici da moltiplicare e calcola una
+ * Un solver prende una certa sequenza di matrici da moltiplicare e calcola una
  * parentesizzazione ottima, cioè che minimizza il numero di moltiplicazioni
  * scalari necessarie per moltiplicare tutte le matrici.
  * 
@@ -45,8 +45,7 @@ public class MatrixMultiplicationSolver {
         if (p == null)
             throw new NullPointerException("Lista nulla");
         if (p.size() <= 1)
-            throw new IllegalArgumentException(
-                    "Lista di dimensione non valida");
+            throw new IllegalArgumentException("Lista di dimensione non valida");
         this.p = p;
         this.m = new int[p.size() - 1][p.size() - 1];
         this.b = new int[p.size() - 1][p.size() - 1];
@@ -58,7 +57,50 @@ public class MatrixMultiplicationSolver {
      * dinamica.
      */
     private void solve() {
-        // TODO implementare
+        // CASO BASE (riempo la diagonale principale con tutti 0)
+        for(int i = 0; i < this.m.length; i++)
+            m[i][i] = 0;
+
+        // CASO RICORSIVO
+        // Primo ciclo deve scorrere tutte le diagonali (partiamo
+        // dalla seconda perché la prima è quella principale ed
+        // è stata già riempita, perciò d = 1 e non d = 0)
+
+        for(int d = 1; d < this.m.length; d++) {
+            // Al suo interno scorriamo tutte le caselle della diagonale
+            // corrente, partendo sempre ovviamente dalla riga 0 e arrivando
+            // a this.m.length - d - 1 (i sarebbe quindi l'indice di RIGA)
+
+            for(int i = 0; i < this.m.length - d; i++) {
+                // Determiniamo l'indice di COLONNA
+                int j = i + d;
+
+                // Inizializziamo la casella corrente m[i][j] con il numero
+                // più grande possibile, in modo che il primo valore calcolato
+                // al variare di k sarà sicuramente MINORE di quello già esistente
+                // e verrà dunque subito inserito (poi si confrontano gli altri)
+
+                this.m[i][j] = Integer.MAX_VALUE; // VALORE DUMMY
+                // (valore “fittizio”, di comodo, messo solo per inizializzare una
+                // variabile, non perché rappresenti un risultato valido del problema)
+
+                // Calcoliamo per ogni casella m[i][j] la ricorrenza
+                // per tutti i valori di k compresi tra i e j - 1 e
+                // determiniamo quella che porta al risultato MINIMO
+                for(int k = i; k < j; k++) {
+                    int result = this.m[i][k] + this.m[k + 1][j] +
+                            (this.p.get(i) * this.p.get(k + 1) * this.p.get(j + 1));
+                    // Confrontiamo nuovo valore con quello più piccolo finora
+                    if(result < this.m[i][j]) {
+                        // Se è minore significa che RESULT corrisponde al
+                        // minimo attuale quindi aggiorniamo m[i][j]
+                        this.m[i][j] = result;
+                        // Aggiorniamo anche b[i][j] con il k corrente
+                        this.b[i][j] = k;
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -94,8 +136,19 @@ public class MatrixMultiplicationSolver {
      * appositamente durante il processo di calcolo del costo minimo
      */
     private String traceBack(int i, int j) {
-        // TODO implementare ricorsivamente
-        return null;
+        StringBuilder s = new StringBuilder();
+        // CASO BASE (una sola matrice)
+        if(i == j) {
+            s.append("A_{").append(i).append("}");
+            return s.toString();
+        }
+        // CASO RICORSIVO (almeno 2 matrici)
+        // Estraiamo il k dalla casella i, j della matrice b
+        int k = this.b[i][j];
+        // Chiamiamo ricorsivamente il metodo sulle caselle
+        // i, k e k + 1, j finché non arriviamo al caso base
+        s.append("(").append(this.traceBack(i, k)).append(" x ").append(this.traceBack(k+1, j)).append(")");
+        return s.toString();
     }
 
 }
